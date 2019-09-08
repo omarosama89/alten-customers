8336c01dd0c1c7f80babdb3c8b673ecb2d249dce
Omar <o.mohammed@morniksa.com>
2019-09-08 11:29:39 +0200

    alter addres field

--- server/models.py
+++ server/models.py
@@ -5,4 +5,4 @@ from django.utils.encoding import python_2_unicode_compatible
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    address = models.CharField(max_length=100)
+    address = models.TextField(max_length=100)
